c49a483f0e2a157de682f71a62050ee991d26919
Lisa Tashtiyeva <tashtiyeva.lisa@gmail.com>
2021-06-28 10:48:55 +0200

    Résolution issue1 : fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,10 +7,13 @@ function editNav() {
   }
 }
 
+//----Gestion de l'affichage de la modale-----//
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose= document.querySelector("span.close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +23,9 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal
+modalClose.addEventListener("click",closeModal);
+function closeModal() {
+  modalbg.style.display = "none"; //permet de ne pas 'rendre visible' la modale"
+};
 
